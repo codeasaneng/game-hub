00946cac44ce64678c89d65914e4fd38faf24b8f
Bruno <bcaeirourbano@gmail.com>
2023-06-14 22:18:09 +0100

    Enable colorMode

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,14 +1,15 @@
-import { HStack, Image, Text } from '@chakra-ui/react'
-import React from 'react'
-import logo from '../assets/logo.webp';
+import { HStack, Image } from "@chakra-ui/react";
+import React from "react";
+import logo from "../assets/logo.webp";
+import ColorModeSwitch from "./ColorModeSwitch";
 
 const NavBar = () => {
   return (
-    <HStack>
-        <Image src={logo} boxSize='60px'/>
-        <Text>NavBar</Text>
+    <HStack justifyContent="space-between">
+      <Image src={logo} boxSize="60px" />
+      <ColorModeSwitch/>
     </HStack>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
